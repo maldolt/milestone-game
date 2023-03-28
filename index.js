ee1a8156439a5d9e0edd084843b7dfc8361bc167
Your Name~ <maldolt@nv.ccsd.net>
2023-03-27 20:52:48 -0700

    testing javaScript

--- index.js
+++ index.js
@@ -1,62 +1,20 @@
-/*possible method from (somvirs57)
-const letterDiv = document.querySelector('.letter-div');
-
-let letters;
-
-const init = function(state) {
-    wordDiv.innerHTML ='';
-    if (state === 'start'){
-        for (const i of 'abcdefghijklmnopqrstuvwxyz') {
-            const html = `<button class = "alpha">${i.toUpperCase()}</button>`;
-            letterDiv.interAjacentHTML('beforeend', html)
-        }
-    } else if (state === 'reset') {
-        letters.forEach(btn => {
-            btn.classList.remove('disabled');
-            notif.classList.add('hidden');
-        });
-    }
-    
-    letters = document.querySelectorAll('.alpha');
-
-}*/
 /* possible method of letters from (cathydutton)*/
 
 window.onload = function () {
-    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
+/* what elements will be needed: word, correct guess, incorrect guess, lives, 
+& how many spaces will be needed for the words */   
+    const word;
+    const guess;
+    const counter;     
+    const lives;
+    const space;
 
-
-    var buttons = function () {
-        myButtons = document.getElementById('buttons');
-        letters = document.createElement('ul');
-
-        for (var i = 0; i < alphabet.length; i++) {
-        letters.id = 'alphabet';
-        list = document.createElement('li');
-        list.id = 'letter';
-        list.innerHTML = alphabet[i];
-        check();
-        myButtons.appendChild(letters);
-        letters.appendChild(list);
-        }
-    }
-    result = function () {
-        wordHolder = document.getElementById('hold');
-        correct = document.createElement('ul');
-
-        for (var i = 0; i < word.length; i++) {
-        correct.setAttribute('id', 'my-word');
-        guess = document.createElement('li');
-        guess.setAttribute('class', 'guess');
-        if (word[i] === "-") {
-            guess.innerHTML = "-";
-            space = 1;
-        } else {
-            guess.innerHTML = "_";
-        }
-
-        }
-    }    
+    /*attempt to get elements*/
+    
 }
+
+
+   

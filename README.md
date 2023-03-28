4d7036b4171e5f84095941f7c1dc9c77990ea97e
Your Name~ <maldolt@nv.ccsd.net>
2023-03-27 21:16:35 -0700

    JavaScript

--- README.md
+++ README.md
@@ -2,3 +2,25 @@ Hang man game about animals for kids ages 6-10
 
 Git hub Resource for Letter formation (cathydutton)
 https://codepen.io/cathydutton/details/JjpxMm
+
+Tasks to do:
+Letters
+-Array for letters and have it acually display
+-make letters into clickable objects
+-disable letters that have been choosen
+Words
+--blank lines where words will go
+-words that will be used: whale, turtle, fish
+-this is the 'hold'
+Lives
+-6 lives
+-need to appear when wrong letter is clicked
+-Parts: head, body, 2 arms, 2 legs
+End of Game
+-pop-up window
+-game over
+    -You won!
+    -You did not win!
+Reset
+-Make play again button function
+
--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 /* possible method of letters from (cathydutton)*/
 
 window.onload = function () {
-    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
 /* what elements will be needed: word, correct guess, incorrect guess, lives, 
 & how many spaces will be needed for the words */   
-    const word;
-    const guess;
-    const counter;     
-    const lives;
-    const space;
+    var word;
+    var guess;
+    var counter;     
+    var lives;
+    var space;
 
+    let lives = 6
+    let words =['whale', 'turtle', 'fish' ]
     /*attempt to get elements*/
-    
-}
 
+}
+    console.log(lives);
 
    

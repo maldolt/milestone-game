dea6cfa78a98a936b5f3aea9a357ec08862572f9
Your Name~ <maldolt@nv.ccsd.net>
2023-03-27 21:58:54 -0700

    Testing var

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ window.onload = function () {
     var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        
 /* what elements will be needed: word, correct guess, incorrect guess, lives, 
-& how many spaces will be needed for the words */   
+& how many spaces will be needed for the words */  
     var word;
     var guess;
     var counter;     
@@ -15,8 +16,8 @@ window.onload = function () {
     let lives = 6
     let words =['whale', 'turtle', 'fish' ]
     /*attempt to get elements*/
-
+console.log(alphabet);
 }
-    console.log(lives);
+    
 
    
